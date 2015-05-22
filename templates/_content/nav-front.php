bda56d3d7966a9ed642f74063be5c0868607f004
astoker88 <alex.stoker@asset.tv>
2015-05-22 14:15:18 +0100

    add nav for front page

---
+++ templates/_content/nav-front.php
@@ -0,0 +1,39 @@
+<div class="navbar navbar-default navbar-front" role="navigation">
+    <div class="container">
+        <div class="navbar-header ">
+            <button type="button" class="navbar-toggle collapsed pull-left toggle-nav" >
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img src="images/logos/ass-Logo.png" alt="Asset.tv" /></a>
+            <!--            <button type="button" class="pull-right watchlist hidden-lg hidden-md hidden-sm" href="#">Watch List</button>-->
+
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav ">
+                <li class="active"><a href="#">CHANNELS</a></li>
+                <li><a href="#about">MARKETS</a></li>
+                <li><a href="#about">TRAINING</a></li>
+            </ul>
+            <ul class="nav navbar-nav hidden-sm second-nav text-uppercase">
+                <li><span class="spacer">|</span></li>
+                <li class="active"><a href="#">Trending</a></li>
+                <li><a href="#about">Newsletter</a></li>
+                <li><a href="#about">Blog</a></li>
+                <li><a href="#about">Feedback</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown login-dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">US<img src="images/icons/login_white.png" /><i class="caret"></i></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><?php include "login.php"; ?></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+
+
