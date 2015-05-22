598ba0770ca3f9dfe5806d8b4b9315c4a7d87cb6
astoker88 <alex.stoker@asset.tv>
2015-05-22 14:23:18 +0100

    update nav for front

--- templates/page/page-front.tpl.php
+++ templates/page/page-front.tpl.php
@@ -1,7 +1,10 @@
 <header id="header">
 
-  <?php theme('templates/_content/nav-front.php'); ?>
+    <?php theme('templates/_content/nav-front.php'); ?>
 
 </header>
 
+<div class="clearfix footer-container">
+   <?php theme('templates/_content/footer.php'); ?>
+</div>
 HOME
