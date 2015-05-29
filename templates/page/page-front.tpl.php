e751146829a50491040804534f5273c24253177e
astoker88 <alex.stoker@asset.tv>
2015-05-29 13:55:32 +0100

    front page template

--- templates/page/page-front.tpl.php
+++ templates/page/page-front.tpl.php
@@ -5,7 +5,7 @@
         <div id="front-nav">
             <?php theme('_content/nav-front.php'); ?>
         </div>
-        
+
         <div class=""><div id="header-nav-btns">
                 <!--<div class="header-item switch featurebox hidden-xs"><img src="images/icons/slider-right.png" /></div>-->
                 <div class="header-item switch searchbox hidden"><img src="images/icons/search-glass.png" /></div>
@@ -13,9 +13,19 @@
             <div id="header-slider-wrapper" class="">
 
                 <div class="header-slider-inner">
-                    <div class="header-item search-wrapper col-xs-6 col-sm-6 col-md-6 col-lg-6"><?php include "components/header-search.php"; ?></div>
+                    <div class="header-item search-wrapper col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <?php
+                        //include "components/header-search.php"; 
+                        <theme('_content/header-search.php'); 
+                        ?>
+                    </div>
                     <!-- Icons need to be converted in fonticon at some point -->
-                    <div class="header-item featuredbox col-xs-6 col-sm-6 col-md-6 col-lg-6 hidden-xs"><?php include "components/header-featured.php"; ?></div>
+                    <div class="header-item featuredbox col-xs-6 col-sm-6 col-md-6 col-lg-6 hidden-xs">
+                        <?php
+                        //include "components/header-featured.php"; 
+                        theme('_content/header-featured.php'); 
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -27,11 +37,18 @@
     <section id="content-top">
 
         <div class="container">
-            <?php include "components/featured-tabs.php"; ?>
+            <?php 
+            //include "components/featured-tabs.php"; 
+            theme('_content/featured-tabs.php'); 
+            ?>
         </div>
 
         <div class="container">
-            <?php include "components/latest-tab.php"; ?>
+            <?php
+            //include "components/latest-tab.php"; 
+             theme('_content/latest-tab.php'); 
+            
+            ?>
 
         </div>
     </section>     
