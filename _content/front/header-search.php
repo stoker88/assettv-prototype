ca6a22647569c7e7fb3f9022dc3f970c99ee0ee0
astoker88 <alex.stoker@asset.tv>
2015-05-29 14:01:33 +0100

    front page template

---
+++ _content/front/header-search.php
@@ -0,0 +1,35 @@
+<div id="search-wrapper" class="col-centered">
+     <div class="header-item switch go-right featurebox hidden-xs"><img src="images/icons/slider-right.png" /></div>
+    <div class="search-title text-center">
+        <h4>1,125 videos from 125 top companies</h4>
+    </div>
+    <div class="row">
+        <section id="search" class="col-centered">
+            <div id="search-box" >
+                <form class="search-form" action="" method="post" id="search-form" accept-charset="UTF-8">
+                    <div>
+                        <div class="input-group form-wrapper">
+                            <div class="form-item form-type-textfield form-item-keys">
+                                <input class="typeahead" type="text" placeholder="Search" autocomplete="off"> 
+                            </div>
+                            <span class="input-group-btn">
+                                <input class="btn btn-default form-submit" type="image" name="submit" src="images/icons/mag_white.png">
+                            </span>
+
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+                <div id="featured-links-wrapper" class="">
+                    <div id="featured-links" class="">
+                        <span><i>Featured links: </i></span><span><strong><a href="#">Equity Funds</a>, <a href="#">Bill Gross</a>, <a href="#">Bond Funds</a></strong></span>
+                    </div>
+                    <div id="featured-sponsor" class="pull-right hidden-xs">
+                        <span class="hidden-sm"><sub>Search is sponsored by </sub></span>
+                        <img title="Search is sponsored by" width="76" src="images/logos/logo.png" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
