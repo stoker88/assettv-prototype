937d8f6e64f648f2f363d8f755b9c3a17af34d05
astoker88 <alex.stoker@asset.tv>
2015-05-29 14:04:00 +0100

    front page template

---
+++ _content/featured-tabs.php
@@ -0,0 +1,19 @@
+<div id="feature-tabs" role="tabpanel">
+      <!-- Nav tabs -->
+  <ul id="featured-tablist" class="nav nav-tabs nav-justified hidden-xs" role="tablist" id="myTab">
+    <li role="presentation" class="tabs vertical-center active"><a href="#company-channels-tab" aria-controls="company-channels-tab" role="tab" data-toggle="tab">Company Channels</a></li>
+    <li role="presentation" class="tabs vertical-center"><a href="#industry-channels-tab" aria-controls="industry-channels-tab" role="tab" data-toggle="tab">Industry Channels</a></li>
+    <li role="presentation" class="tabs vertical-center"><a href="#masterclass-tab" aria-controls="masterclass-tab" role="tab" data-toggle="tab">Masterclass</a></li>
+    <li role="presentation" class="tabs vertical-center"><a href="#academy-tab" aria-controls="academy-tab" role="tab" data-toggle="tab">The Academy</a></li>
+    <li role="presentation" class="tabs vertical-center"><a href="#hot-topics-tab" aria-controls="hot-topics-tab" role="tab" data-toggle="tab">Hot topics</a></li>
+  </ul>
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="company-channels-tab"><?php include "company-slider.php"; ?></div>
+    <div role="tabpanel" class="tab-pane" id="industry-channels-tab"><?php include "channels-industry.php"; ?></div>
+    <div role="tabpanel" class="tab-pane" id="masterclass-tab"><?php include "channels-masterclass.php"; ?></div>
+    <div role="tabpanel" class="tab-pane" id="academy-tab"><?php include "channels-academy.php"; ?></div>
+    <div role="tabpanel" class="tab-pane" id="hot-topics-tab"><?php include "channels-hottopics.php"; ?></div>
+  </div>
+
+</div>
---
+++ _content/latest-tab.php
@@ -0,0 +1,15 @@
+<div role="tabpanel">
+
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs latest-tab" role="tablist">
+    <li role="presentation" class="active"><a href="#latest-content-tab" aria-controls="latest-content-tab" role="tab" data-toggle="tab">Latest content</a></li>
+    <li role="presentation"><a href="#trending-tab" aria-controls="trending-tab" role="tab" data-toggle="tab">Trending</a></li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="latest-content-tab"><?php include "latest-items.php"; ?></div>
+    <div role="tabpanel" class="tab-pane" id="trending-tab"><?php include "trending-items.php"; ?></div>
+  </div>
+
+</div>
