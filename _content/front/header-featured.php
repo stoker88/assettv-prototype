3d71eb728724e7571ce68889d59de0aaf7ccd403
astoker88 <alex.stoker@asset.tv>
2015-05-29 14:03:15 +0100

    front page template

---
+++ _content/front/header-featured.php
@@ -0,0 +1,63 @@
+<div id="header-featured" class="carousel slide row" data-ride="carousel">
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner custom-carousel-inner" role="listbox">
+
+        <div class="feature-wrapper item active">
+
+            <img class="main-thumb image-container img-responsive row col-sm-4 col-xs-4 col-lg-5 col-md-5" src="https://www.assettvus.com/sites/default/files/video/images/masterclass_small_cap_equities_-_july_2014.PNG" />
+
+
+            <div class="item-info-container col-lg-7 col-md-7 col-sm-8 col-xs-8">
+
+                <div class="item-info">
+                    <h2>Risk-Managed Participation Accross Market Cycles in the States</h2>
+                    <span class="date text-uppercase">September 2015</span>
+                </div>
+                <div class="company-info-container" style="background-color: #482b45;">
+                    <a class="company-logo-link" href="#" style="background-image: url(images/logos/hp-logos_10.jpg)"></a>
+                    <div class="company-info">
+                        <h5>Guggenheim Asset is a global investment management company</h5>
+                        <a href="#">Visit Guggenheim Channel</a>
+                    </div>     
+                </div>
+            </div>
+        </div>
+
+
+        <div class="feature-wrapper item">
+
+            <img class="main-thumb image-container img-responsive row col-sm-4 col-xs-4 col-lg-5 col-md-5" src="https://assettvus.com/sites/default/files/video/images/currency-hedged_etfs.png" />
+
+
+            <div class="item-info-container col-lg-7 col-md-7 col-sm-8 col-xs-8">
+
+                <div class="item-info">
+                    <h2>Risk-Managed Participation Accross Market Cycles in the States</h2>
+                    <span class="date text-uppercase">September 2015</span>
+                </div>
+                <div class="company-info-container" style="background-color: #482b45;">
+                    <a class="company-logo-link" href="#" style="background-image: url(images/logos/hp-logos_10.jpg)"></a>
+                    <div class="company-info">
+                        <h5>Guggenheim Asset is a global investment management company</h5>
+                        <a href="#">Visit Guggenheim Channel</a>
+                    </div>     
+                </div>
+            </div>
+        </div> 
+
+
+
+    </div>
+    <!-- Left and right controls -->
+    <div id="control-wrapper" class="container">
+        <a class="left carousel-control custom-control" href="#header-featured" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control custom-control" href="#header-featured" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
