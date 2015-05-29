2b8b95c4b1f65dd9898a1f213f1374637a511972
astoker88 <alex.stoker@asset.tv>
2015-05-29 14:13:30 +0100

    front page template

--- _content/latest-tab.php
+++ _content/latest-tab.php
@@ -1,15 +1,24 @@
 <div role="tabpanel">
 
-  <!-- Nav tabs -->
-  <ul class="nav nav-tabs latest-tab" role="tablist">
-    <li role="presentation" class="active"><a href="#latest-content-tab" aria-controls="latest-content-tab" role="tab" data-toggle="tab">Latest content</a></li>
-    <li role="presentation"><a href="#trending-tab" aria-controls="trending-tab" role="tab" data-toggle="tab">Trending</a></li>
-  </ul>
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs latest-tab" role="tablist">
+        <li role="presentation" class="active"><a href="#latest-content-tab" aria-controls="latest-content-tab" role="tab" data-toggle="tab">Latest content</a></li>
+        <li role="presentation"><a href="#trending-tab" aria-controls="trending-tab" role="tab" data-toggle="tab">Trending</a></li>
+    </ul>
 
-  <!-- Tab panes -->
-  <div class="tab-content">
-    <div role="tabpanel" class="tab-pane active" id="latest-content-tab"><?php include "latest-items.php"; ?></div>
-    <div role="tabpanel" class="tab-pane" id="trending-tab"><?php include "trending-items.php"; ?></div>
-  </div>
+    <!-- Tab panes -->
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="latest-content-tab">
+            <?php 
+            include "latest-items.php"; 
+         
+            theme_view('channel', 'video-list', '_content/channel/video_list.php', '_content/channel/video.php', null, '_content/channel/filter_menu.php');
+            
+            ?>
+        </div>
+        <div role="tabpanel" class="tab-pane" id="trending-tab">
+            <?php include "trending-items.php"; ?>
+        </div>
+    </div>
 
 </div>
