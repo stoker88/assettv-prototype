d10f4f9fefb2e5d10f715a5a0acd59e5c8b28148
astoker88 <alex.stoker@asset.tv>
2015-06-01 15:35:28 +0100

    remove includes

---
+++ _content/front/video_list.php
@@ -0,0 +1,36 @@
+<div class="views-row views-row-first">
+  <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row">
+  <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row">
+  <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row">
+  <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row">
+  <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row">
+  <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row">
+  <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row">
+  <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row">
+  <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row">
+ <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row">
+ <?php theme_node('video-front', 'teaser'); ?>
+</div>
+<div class="views-row views-row-last">
+  <?php theme_node('video-front', 'teaser'); ?>
+</div>
