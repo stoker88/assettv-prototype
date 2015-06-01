85003887abfb36bf5bc83dc8c9cb7871b41e3313
astoker88 <alex.stoker@asset.tv>
2015-06-01 17:29:02 +0100

    update templates

--- _content/front/channels-masterclass.php
+++ _content/front/channels-masterclass.php
@@ -1,7 +1,7 @@
 <div id="item-list-channel-academy">
     
     <ul class="items term-list  list-unstyled">
-        
+        <?php theme('templates/_content/channel_item');?>
     </ul>
     
      <div class="clearfix"></div>
