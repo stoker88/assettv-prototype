882780603f02f4fa5115b75c178e03811892f0f1
astoker88 <alex.stoker@asset.tv>
2015-06-01 15:03:00 +0100

    remove includes

--- templates/page/page-front.tpl.php
+++ templates/page/page-front.tpl.php
@@ -1,56 +1,52 @@
 <div id="body-container">
 
-  <header id=header class="">
+    <header id=header class="">
 
-    <div id="front-nav">
-      <?php theme('_content/nav-front.php'); ?>
-    </div>
-
-    <div class=""><div id="header-nav-btns">
-            <!--<div class="header-item switch featurebox hidden-xs"><img src="images/icons/slider-right.png" /></div>-->
-        <div class="header-item switch searchbox hidden"><img src="images/icons/search-glass.png" /></div>
-      </div>
-      <div id="header-slider-wrapper" class="">
-
-        <div class="header-slider-inner">
-          <div class="header-item search-wrapper col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <?php
-            //include "components/header-search.php"; 
-            theme('_content/front/header-search.php');
-            ?>
-          </div>
-          <!-- Icons need to be converted in fonticon at some point -->
-          <div class="header-item featuredbox col-xs-6 col-sm-6 col-md-6 col-lg-6 hidden-xs">
-            <?php
-            //include "components/header-featured.php"; 
-            theme('_content/front/header-featured.php');
-            ?>
-          </div>
+        <div id="front-nav">
+            <?php theme('_content/nav-front.php'); ?>
         </div>
-      </div>
-    </div>
+
+        <div class=""><div id="header-nav-btns">
+                <!--<div class="header-item switch featurebox hidden-xs"><img src="images/icons/slider-right.png" /></div>-->
+                <div class="header-item switch searchbox hidden"><img src="images/icons/search-glass.png" /></div>
+            </div>
+            <div id="header-slider-wrapper" class="">
+
+                <div class="header-slider-inner">
+                    <div class="header-item search-wrapper col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <?php
+                        theme('_content/front/header-search.php');
+                        ?>
+                    </div>
+                    <!-- Icons need to be converted in fonticon at some point -->
+                    <div class="header-item featuredbox col-xs-6 col-sm-6 col-md-6 col-lg-6 hidden-xs">
+                        <?php
+                        theme('_content/front/header-featured.php');
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
-  </header>
+    </header>
 
-  <section id="content-top">
+    <section id="content-top">
 
-    <div class="container">
-      <?php
-      //include "components/featured-tabs.php"; 
-      theme('_content/featured-tabs.php');
-      ?>
-    </div>
+        <div class="container">
+            <?php
+            theme('_content/featured-tabs.php');
+            ?>
+        </div>
 
-    <div class="container">
-      <?php
-      //include "components/latest-tab.php"; 
-      theme('_content/latest-tab.php');
-      ?>
+        <div class="container">
+            <?php
+            theme('_content/latest-tab.php');
+            ?>
 
-    </div>
-  </section>     
+        </div>
+    </section>     
 
 
 
@@ -59,5 +55,5 @@
 
 <!-- Footer Region -->
 <footer class="region region-footer">
-  <?php theme('_content/footer.php'); ?>
+    <?php theme('_content/footer.php'); ?>
 </footer>
