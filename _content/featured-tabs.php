81c97d7937b9305dd086c8457e84f306e8400eaf
astoker88 <alex.stoker@asset.tv>
2015-06-01 16:55:13 +0100

    add front less

--- _content/featured-tabs.php
+++ _content/featured-tabs.php
@@ -9,11 +9,22 @@
   </ul>
   <!-- Tab panes -->
   <div class="tab-content">
-    <div role="tabpanel" class="tab-pane active" id="company-channels-tab"><?php //include "company-slider.php"; ?></div>
-    <div role="tabpanel" class="tab-pane" id="industry-channels-tab"><?php include "channels-industry.php"; ?></div>
-    <div role="tabpanel" class="tab-pane" id="masterclass-tab"><?php include "channels-masterclass.php"; ?></div>
-    <div role="tabpanel" class="tab-pane" id="academy-tab"><?php include "channels-academy.php"; ?></div>
-    <div role="tabpanel" class="tab-pane" id="hot-topics-tab"><?php include "channels-hottopics.php"; ?></div>
+    <div role="tabpanel" class="tab-pane active" id="company-channels-tab">
+        <?php include "company-slider.php"; ?>
+        <?php theme_view('channel', 'channel-list', '_content/front/company_slider.php',null, null, null);?>
+    </div>
+    <div role="tabpanel" class="tab-pane" id="industry-channels-tab">
+        <?php include "channels-industry.php"; ?>
+    </div>
+    <div role="tabpanel" class="tab-pane" id="masterclass-tab">
+        <?php include "channels-masterclass.php"; ?>
+    </div>
+    <div role="tabpanel" class="tab-pane" id="academy-tab">
+        <?php include "channels-academy.php"; ?>
+    </div>
+    <div role="tabpanel" class="tab-pane" id="hot-topics-tab">
+        <?php include "channels-hottopics.php"; ?>
+    </div>
   </div>
 
 </div>
