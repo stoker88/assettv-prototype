9711feac3e9917c7845d5861a7797568d32e0851
astoker88 <alex.stoker@asset.tv>
2015-06-01 16:53:30 +0100

    add front less

---
+++ _content/front/company-slider.php
@@ -0,0 +1,19 @@
+<div id="companyCarousel">
+    <div class="text-center">
+
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+<?php theme_node('channel', 'teaser'); ?>
+        
+    </div>
+</div>
+
