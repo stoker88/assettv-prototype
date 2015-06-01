d5ddade9c85196dc4361c4f9065f9659682b01ab
astoker88 <alex.stoker@asset.tv>
2015-06-01 17:31:01 +0100

    update templates

--- _content/front/channels-industry.php
+++ _content/front/channels-industry.php
@@ -1,7 +1,19 @@
-<div id="item-list-channel-academy">
+<div id="item-list-channel-industry">
     
-      <ul class="items term-list  list-unstyled">
-        
+     <ul class="items term-list  list-unstyled">
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+        <?php theme('templates/_content/channel-item.php');?>
+       
     </ul>
     
      <div class="clearfix"></div>
