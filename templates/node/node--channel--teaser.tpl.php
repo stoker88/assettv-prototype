1eddc46654fbe0ce82c4a4584e4b33c4d40df746
astoker88 <alex.stoker@asset.tv>
2015-06-02 16:34:59 +0100

    update modules

--- templates/node/node--channel--teaser.tpl.php
+++ templates/node/node--channel--teaser.tpl.php
@@ -79,16 +79,11 @@
  * @ingroup themeable
  */
 ?>
-<article id="node-****" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-  <div class="content"<?php print $content_attributes; ?>>
-    <!-- print render($content); -->
-    <div class="channel-item text-left" content-type="video">
-      <a href="#">
+<!-- print render($content); -->
+<div class="channel-item text-left" content-type="video">
+    <a href="#">
         <img src="images/logos/hp-logos_06.jpg" class="img-responsive">
         <span class="key">3 Videos</span> 
-      </a>
-    </div>
-  </div>
+    </a>
+</div>
 
-</article>
