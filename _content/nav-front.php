f2bea7be156bbc07cc15249aed24a453986bc2cd
astoker88 <alex.stoker@asset.tv>
2015-06-02 15:51:47 +0100

    add image

--- _content/nav-front.php
+++ _content/nav-front.php
@@ -1,31 +1,50 @@
 <div class="navbar navbar-default navbar-front" role="navigation">
-  <div class="container">
-    <div class="navbar-header ">
-      <button type="button" class="navbar-toggle collapsed pull-left toggle-nav" >
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#"><img src="images/assettv.png" alt="Asset.tv" /></a>
-    </div>
-    <nav id="navbar" class="collapse navbar-collapse">
-      <ul class="nav navbar-nav ">
-        <li class="active"><a href="#">CHANNELS</a></li>
-        <li><a href="#about">MARKETS</a></li>
-        <li><a href="#about">TRAINING</a></li>
-      </ul>
-      <ul class="nav navbar-nav hidden-sm second-nav text-uppercase">
-        <li><span class="spacer">|</span></li>
-        <li class="active"><a href="#">Trending</a></li>
-        <li><a href="#about">Newsletter</a></li>
-        <li><a href="#about">Blog</a></li>
-        <li><a href="#about">Feedback</a></li>
-      </ul>
-      <?php theme_block('block-menu-user-menu', '_content/usermenu.php', '', 'block-menu'); ?>
+    <div class="container">
+        <div class="navbar-header ">
+            <button type="button" class="navbar-toggle collapsed pull-left toggle-nav" >
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img src="images/assettv.png" alt="Asset.tv" /></a>
+        </div>
+        <nav id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav ">
+                <li class="active"><a href="#">CHANNELS</a></li>
+                <li><a href="#about">MARKETS</a></li>
+                <li><a href="#about">TRAINING</a></li>
+            </ul>
+            <ul class="nav navbar-nav hidden-sm second-nav text-uppercase">
+                <li><span class="spacer">|</span></li>
+                <li class="active"><a href="#">Trending</a></li>
+                <li><a href="#about">Newsletter</a></li>
+                <li><a href="#about">Blog</a></li>
+                <li><a href="#about">Feedback</a></li>
+            </ul>
 
-    </nav><!--/.nav-collapse -->
-  </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav ">
+                    <li class="active"><a href="#">CHANNELS</a></li>
+                    <li><a href="#about">MARKETS</a></li>
+                    <li><a href="#about">TRAINING</a></li>
+                </ul>
+                <ul class="nav navbar-nav hidden-sm second-nav text-uppercase">
+                    <li><span class="spacer">|</span></li>
+                    <li class="active"><a href="#">Trending</a></li>
+                    <li><a href="#about">Newsletter</a></li>
+                    <li><a href="#about">Blog</a></li>
+                    <li><a href="#about">Feedback</a></li>
+                </ul>
+                <?php theme_block('block-menu-user-menu', '_content/usermenu.php', '', 'block-menu'); ?>
+            </div>
+
+
+
+
+
+        </nav><!--/.nav-collapse -->
+    </div>
 </div>
 
 
