3f3ff7694542bc9f7439646b1abe4ea088492367
astoker88 <alex.stoker@asset.tv>
2015-06-03 10:45:54 +0100

    update css

--- templates/page/page-front.tpl.php
+++ templates/page/page-front.tpl.php
@@ -4,30 +4,33 @@
 
         <div id="front-nav">
             <?php theme('_content/nav-front.php'); ?>
-        </div>
 
-        <div class="">
-            <div id="header-nav-btns">
-                <!--<div class="header-item switch featurebox hidden-xs"><img src="images/icons/slider-right.png" /></div>-->
-                <div class="header-item switch searchbox hidden"><img src="images/icons/search-glass.png" /></div>
-            </div>
-            <div id="header-slider-wrapper" class="">
+            <div class="">
+                <div id="header-nav-btns">
+                    <!--<div class="header-item switch featurebox hidden-xs"><img src="images/icons/slider-right.png" /></div>-->
+                    <div class="header-item switch searchbox hidden"><img src="images/icons/search-glass.png" /></div>
+                </div>
+                <div id="header-slider-wrapper" class="">
 
-                <div class="header-slider-inner">
-                    <div class="header-item search-wrapper col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                        <?php
-                        theme('_content/front/header-search.php');
-                        ?>
-                    </div>
-                    <!-- Icons need to be converted in fonticon at some point -->
-                    <div class="header-item featuredbox col-xs-6 col-sm-6 col-md-6 col-lg-6 hidden-xs">
-                        <?php
-                        theme('_content/front/header-featured.php');
-                        ?>
+                    <div class="header-slider-inner">
+                        <div class="header-item search-wrapper col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <?php
+                            theme('_content/front/header-search.php');
+                            ?>
+                        </div>
+                        <!-- Icons need to be converted in fonticon at some point -->
+                        <div class="header-item featuredbox col-xs-6 col-sm-6 col-md-6 col-lg-6 hidden-xs">
+                            <?php
+                            theme('_content/front/header-featured.php');
+                            ?>
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
+
+
 
 
 
